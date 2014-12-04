880da37011207768ff60d9d50b2952c16ce4ea99
Vladimír Oraný <vladimir@orany.cz>
2014-12-04 16:20:22 +0100

    frontend tool configs

---
+++ karma.conf.js
@@ -0,0 +1,50 @@
+// an example karma.conf.js
+module.exports = function(config) {
+    config.set({
+        basePath: '.',
+        frameworks: ['jasmine'],
+
+        browsers: [
+            // for CI
+            // 'PhantomJS',
+            'Chrome'
+        ],
+        reporters: ['progress', 'junit'],
+        // set true for CI
+        singleRun: false,
+        // set false for CI
+        autoWatch : true,
+
+        junitReporter: {
+            outputFile: 'target/reports/js/karma-test-results.xml',
+            suite: 'unit'
+        },
+
+        files: [
+            // Required libraries
+            'grails-app/assets/bower_components/jquery/dist/jquery.js',
+            'grails-app/assets/bower_components/angular/angular.js',
+
+            // App under test
+            'grails-app/assets/javascripts/**/*.js',
+
+            // Tests
+            'grails-app/assets/bower_components/angular-mocks/angular-mocks.js',
+
+            // Fixtures from Backend
+            'test/js/**/*.fixture.js',
+
+            // Tests
+            'test/js/**/*.!(fixture.)js'
+        ],
+
+        plugins: [
+            'karma-jasmine',
+            'karma-chrome-launcher',
+            // for CI
+            // 'karma-phantomjs-launcher',
+            'karma-junit-reporter'
+        ]
+
+    });
+};
